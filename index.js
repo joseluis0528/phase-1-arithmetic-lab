2cf380455350f2540506596bc860783ff600f37e
Jose <joseluis0528@gmail.com>
2023-03-14 01:28:05 -0500

    complete lab

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ let num1 = 31;
 let num2 = 2;
 let multiply = num1 * num2;
 
-let random = Math.floor(Math.random() * 10) + 1
+let random = Math.floor(Math.random() * 10) + 1;
 
 let num3 = 16;
 let num4 = 4;
